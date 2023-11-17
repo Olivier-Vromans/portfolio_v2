0057588c63cddee23fdbbc704733855e5b9a3d0d
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-17 23:58:47 +0100

    change z-index + split on space

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
     const navItems = ["Skills", "Projects", "Work Experience", "Contact"];
 
     return (
-        <nav className='absolute top-0 w-full flex items-center justify-between flex-wrap p-6 gap-5'>
+        <nav className='absolute z-50 top-0 w-full flex items-center justify-between flex-wrap p-6 gap-5'>
             <div className='flex items-center flex-shrink-0 mr-6'>
                 <p className='text-2xl'>
                     Olivier Vromans
@@ -36,7 +36,7 @@ export default function Header() {
                 <div className='w-full flex flex-col items-center sm:flex-row sm:items-center gap-5 sm:py-0 px-10'>
                     {navItems.map((item, index) => (
                         <div key={index} className='flex items-center gap-3 flex-1 self-stretch rounded-2xl border bg-linearColor border-custom/10 shadow-custom backdrop-blur-sm  sm:border-0'>
-                            <a href={`#${item.toLowerCase()}`} className='flex-1 text-center py-2'>
+                            <a href={`#${item.toLowerCase().split(' ')[0]}`} className='flex-1 text-center py-2' onClick={() => setMenuOpen(!menuOpen)}>
                                 {item}
                             </a>
                         </div>
