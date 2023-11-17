beef9664ade17e400a24377b811b686e7a2898e5
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-18 00:00:44 +0100

    Edit onClick and new style + size and min width

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
-export default function Button({ text, styleType = 'default' }) {
+export default function Button({ text, className, styleType = 'default', sizeType = 'default', onClick}) {
     const styles = {
         default: "text-quaternary bg-primary hover:bg-quaternary hover:text-primary",
-        secondary: "bg-secondary",
-        outline: "border-primary border",
+        selected: "bg-quaternary text-primary",
+        outline: "border-primary border hover:bg-quaternary hover:text-primary",
     };
 
+    const size = {
+        default: "py-2 px-4",
+        big: "py-4 px-6"
+    }
+
     const selectedStyle = styles[styleType] || styles.default;
+    const selectedSize = size[sizeType] || size.default
 
     return (
-        <div className={`${selectedStyle} py-2 px-4 rounded-full cursor-pointer`}>
+        <button type='button' className={`min-w-[128px] ${className} ${selectedStyle} ${selectedSize} rounded-full cursor-pointer inline-block `} onClick={onClick}>
             {text}
-        </div>
+        </button>
     );
 }
