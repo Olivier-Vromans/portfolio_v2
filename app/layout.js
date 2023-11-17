b37d944080a32edc2176466b2ba167218496c168
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-17 23:59:35 +0100

    Edit extending style + better font download

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaisei+Tokumin&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" />
+      </head>
       <body className={`${inter.className} bg-background`}>
         <Header />
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,25 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      primary: 'var(--color-primary)',
-      secondary: 'var(--color-secondary)',
-      background: "var(--color-background)",
-      tertiaire: "var(--color-tertiaire)",
-      quaternary: "var(--color-quaternary)"
-    },
-    extend:{
-      borderColor:{
+    extend: {
+      colors:{
+        primary: 'rgba(var(--color-primary), <alpha-value>)',
+        secondary: 'rgba(var(--color-secondary), <alpha-value>)',
+        background: "rgba(var(--color-background), <alpha-value>)",
+        tertiaire: "rgba(var(--color-tertiaire), <alpha-value>)",
+        quaternary: "rgba(var(--color-quaternary), <alpha-value>)"
+      },
+      fontFamily: {
+        noto: ['Noto Sans', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+        kaisei: ['Kaisei Tokumin', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      },
+      borderColor: {
         custom: "rgba(var(--color-offWhite), <alpha-value>)",
       },
-      backgroundImage:{
+      backgroundImage: {
         'linearColor': 'linear-gradient(257deg, rgba(252, 252, 253, 0.04) 4.68%, rgba(255, 255, 255, 0.00) 70.6%)',
       },
-      boxShadow:{
+      boxShadow: {
         "custom": "0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
       }
     }
