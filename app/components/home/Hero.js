bcb06569a6b674383a93dbf6f84b41c1ac77ef1a
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-18 16:00:08 +0100

    edit BgMask, fix width

--- app/components/home/Hero.js
+++ app/components/home/Hero.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import Button from '../Button.js';
+import Bgmask from '../Bgmask.js';
 
 export default function Hero() {
     return (
-        <div className='relative h-screen w-screen overflow-hidden'>
-            <div className='absolute inset-0 bg-cover h-screen w-screen' style={{
-                backgroundImage: 'url(/img/hero-bg.png)',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-            }} />
-            <div className='absolute inset-0' style={{
-                backgroundImage: 'linear-gradient(180deg, rgba(var(--color-background), 1) 0%,rgba(var(--color-background), 0.60) 14%, rgba(217, 217, 217, 0) 41%, rgba(217, 217, 217, 0) 73%, rgba(var(--color-background), 1) 100%)',
-            }} />
-            <div className='absolute inset-0' style={{
-                backgroundImage: 'linear-gradient(240deg, rgba(217, 217, 217, 0) 6%, #33242E 70%)'
-            }} />
+        <div className='relative h-screen w-full'>
+            <Bgmask
+                url='/img/hero-bg.png'
+                gradient1='linear-gradient(180deg, rgba(var(--color-background), 1) 0%,rgba(var(--color-background), 0.60) 14%, rgba(217, 217, 217, 0) 41%, rgba(217, 217, 217, 0) 73%, rgba(var(--color-background), 1) 100%)'
+                gradient2='linear-gradient(240deg, rgba(217, 217, 217, 0) 6%, #33242E 70%)' />
 
-            <div className="absolute flex flex-col justify-center h-screen mx-6 gap-8">
+            <div className="relative container mx-auto flex flex-col justify-center h-full gap-8 p-6 sm:p-0">
                 <h1 className="mb-4">Olivier Vromans Portfolio</h1>
                 <div className='flex flex-col gap-6'>
                     <p>
