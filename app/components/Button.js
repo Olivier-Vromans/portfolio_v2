61297acd215cd4c4e7e4c9ad526d24496be91398
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-18 15:59:30 +0100

    edit autocorrection

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,22 +1,49 @@
 import React from 'react';
 
-export default function Button({ text, className, styleType = 'default', sizeType = 'default', onClick}) {
+/**
+ * @typedef {'default' | 'selected' | 'outline'} StyleType
+ */
+
+/**
+ * @typedef {'default' | 'big'} SizeType
+ */
+
+/**
+ * Button component props
+ * @typedef {object} ButtonProps
+ * @property {string} text - The text to be displayed on the button.
+ * @property {string} [className] - Additional CSS classes for the button.
+ * @property {StyleType} [styleType='default'] - The style type for the button.
+ * @property {SizeType} [sizeType='default'] - The size type for the button.
+ * @property {() => void} [onClick] - Callback function for button click.
+ */
+
+/**
+ * Button component
+ * @param {ButtonProps} props - Component props.
+ * @returns {JSX.Element} - Rendered component.
+ */
+export default function Button({ text, className, styleType = 'default', sizeType = 'default', onClick }) {
     const styles = {
         default: "text-quaternary bg-primary hover:bg-quaternary hover:text-primary",
         selected: "bg-quaternary text-primary",
         outline: "border-primary border hover:bg-quaternary hover:text-primary",
     };
 
-    const size = {
+    const sizes = {
         default: "py-2 px-4",
-        big: "py-4 px-6"
-    }
+        big: "py-4 px-6",
+    };
 
     const selectedStyle = styles[styleType] || styles.default;
-    const selectedSize = size[sizeType] || size.default
+    const selectedSize = sizes[sizeType] || sizes.default;
 
     return (
-        <button type='button' className={`min-w-[128px] ${className} ${selectedStyle} ${selectedSize} rounded-full cursor-pointer inline-block `} onClick={onClick}>
+        <button
+            type='button'
+            className={`min-w-[128px] ${className} ${selectedStyle} ${selectedSize} rounded-full cursor-pointer inline-block`}
+            onClick={onClick}
+        >
             {text}
         </button>
     );
