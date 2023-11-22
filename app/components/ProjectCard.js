1c9693c17c24bb298dbb6c6d4b8bb7d035664bf2
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-22 20:24:08 +0100

    edit redone the styling to be responsive

--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -14,35 +14,40 @@ export default function ProjectCard({ project }) {
 
   const buttons = [];
   if (liveLink) {
-    buttons.push(<Button key="live" icon='External' styleType="outline" href={liveLink} />);
+    buttons.push(<Button key="live" icon='External' href={liveLink} />);
   }
   if (githubLink) {
-    buttons.push(<Button key="github" icon="Github" styleType="outline" href={githubLink} />);
+    buttons.push(<Button key="github" icon="Github" href={githubLink} />);
   }
 
   return (
-    <div className="w-72 h-96 p-3 bg-linearColor rounded-2xl shadow border border-neutral-50 border-opacity-10 backdrop-blur-sm justify-start items-start gap-4 flex-col">
-      <Image src={image} alt={title} width={3072} height={2048} objectFit='cover' className="rounded-t-md" />
-      <div className=" h-56 flex flex-col justify-start items gap-4">
-        <div className="text-xl">{title}</div>
-        <div className="self-stretch justify-start items-start gap-1 ">
-          {skills.map((skill, index) => (
-            <div key={index} className="flex-col justify-start items-start inline-flex">
-              <div className="px-2 py-0.5 rounded-md border border-zinc-700 justify-start items-center gap-1 ">
-                <div className="text-zinc-200 text-xs">{skill}</div>
+    <div className="flex flex-col max-w-xs max-h-full gap-4 sm:flex-row md:flex-col lg:flex-row xl:flex-col sm:max-w-md md:max-w-xs lg:max-w-sm  xl:max-w-xs p-4 justify-evenly rounded-2xl shadow border border-neutral-50 border-opacity-10 backdrop-blur-sm bg-linearColor">
+      <div className="relative w-full h-full aspect-square sm:w-1/2 md:w-full lg:w-1/2 xl:w-full ">
+        <Image
+          src={image}
+          alt={title}
+          fill
+          className="rounded-t-md lg:rounded-md object-cover"
+        />
+      </div>
+      <div className="flex-1 h-48 sm:h-full xl:h-48 flex flex-col justify-between gap-4">
+        <div className='flex flex-col justify-between gap-4'>
+          <div className="text-xl">{title}</div>
+          <div className="flex self-stretch justify-start items-start gap-2">
+            {skills.map((skill, index) => (
+              <div key={index} className="flex-col justify-start items-start inline-flex">
+                <div className="px-2 py-0.5 rounded-md border border-zinc-700 justify-start items-center gap-1">
+                  <div className="text-zinc-200 text-xs">{skill}</div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className="text-sm">{desciption}</div>
         </div>
-        <div className="text-sm">{desciption}</div>
         <div className="flex flex-row gap-3 justify-evenly">
-          {/* {[live, github].map((icon, index) => (
-            <Button key={index} icon={icon}  styleType="outline" />
-          ))} */}
           {buttons}
         </div>
       </div>
     </div>
   );
 }
-
