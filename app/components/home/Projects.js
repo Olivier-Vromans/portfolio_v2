9b9eb75cd774c8256c8f178e3d9099352b487d5d
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-22 20:24:40 +0100

    edit better layout and sizing of container

--- app/components/home/Projects.js
+++ app/components/home/Projects.js
@@ -10,13 +10,38 @@ export default function Projects() {
             liveLink: 'https://google.com',
             githubLink: 'https://github.com',
             skills: ['NextJS', 'Docker', 'React']
+        },
+        {
+            title: 'Project 1',
+            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: '/img/placeholder.webp',
+            liveLink: 'https://google.com',
+            githubLink: 'https://github.com',
+            skills: ['NextJS', 'Docker', 'React']
+        },
+        {
+            title: 'Project 1',
+            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: '/img/placeholder.webp',
+            liveLink: 'https://google.com',
+            githubLink: 'https://github.com',
+            skills: ['NextJS', 'Docker', 'React']
+        },
+        {
+            title: 'Project 1',
+            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: '/img/placeholder.webp',
+            liveLink: 'https://google.com',
+            githubLink: 'https://github.com',
+            skills: ['NextJS', 'Docker', 'React']
         }
     ]
 
     return (
-        <div id='projects' className='relative container mx-auto flex flex-col '>
-
-            <div className='mt-8 text-center min-w-8 self-center'>
+        <div id='projects' className='container
+        relative  h-full min-h-screen flex items-center flex-col gap-8
+        '>
+            <div className='mt-8 text-center self-center'>
                 <h2 className='text-2xl'>
                     My Projects
                 </h2>
@@ -24,12 +49,12 @@ export default function Projects() {
                     Check out some of my work
                 </p>
                 <p className='text-base'>
-                    As a web developer, I possess a wide range of skills that allow me to design, build, and maintain websites that are both functional and visually appealing. I have experience with front-end and back-end technologies, as well as web optimization techniques, web accessibility standards, and version control tools. (Replace This Random Text)
+                    As a web developer, I possess a wide range of skills that allow me to design, build, and maintain websites that are both functional and visually appealing. I have experience with front-end and back-end technologies, as well as web optimization techniques, web accessibility standards, and version control tools.
                 </p>
 
             </div>
 
-            <div className='flex flex-col items-center w-full mt-8'>
+            <div className='flex flex-wrap w-full justify-center gap-4'>
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
