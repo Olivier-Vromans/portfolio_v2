2a7b8a50e05a723d2c57752e3394931e359f140f
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-11-24 13:06:31 +0100

    Edit change in Projects

--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -5,11 +5,12 @@ import Button from './Button.js';
 export default function ProjectCard({ project }) {
   const {
     title,
-    desciption,
+    summary,
+    description,
     image,
     liveLink,
     githubLink,
-    skills
+    tags,
   } = project;
 
   const buttons = [];
@@ -20,11 +21,21 @@ export default function ProjectCard({ project }) {
     buttons.push(<Button key="github" icon="Github" href={githubLink} />);
   }
 
+  // TODO - Check if I want This
+  // // Function to shuffle an array and return the first 3 unique elements
+  // const shuffle = (array) => {
+  //   const shuffled = array.sort(() => 0.5 - Math.random());
+  //   return [...new Set(shuffled)];
+  // };
+
+  // // Randomize the order of the tags sorted on alphabetical order
+  // const randomTags = shuffle(tags).slice(0,3).sort();
+
   return (
     <div className="flex flex-col max-w-xs max-h-full gap-4 sm:flex-row md:flex-col lg:flex-row xl:flex-col sm:max-w-md md:max-w-xs lg:max-w-sm  xl:max-w-xs p-4 justify-evenly rounded-2xl shadow border border-neutral-50 border-opacity-10 backdrop-blur-sm bg-linearColor">
       <div className="relative w-full h-full aspect-square sm:w-1/2 md:w-full lg:w-1/2 xl:w-full ">
         <Image
-          src={image}
+          src={image ? `/img/projects/${image}` : '/img/projects/placeholder.webp'}
           alt={title}
           fill
           className="rounded-t-md lg:rounded-md object-cover"
@@ -34,15 +45,15 @@ export default function ProjectCard({ project }) {
         <div className='flex flex-col justify-between gap-4'>
           <div className="text-xl">{title}</div>
           <div className="flex self-stretch justify-start items-start gap-2">
-            {skills.map((skill, index) => (
+            {tags.sort().slice(0,3).map((tag, index) => (
               <div key={index} className="flex-col justify-start items-start inline-flex">
                 <div className="px-2 py-0.5 rounded-md border border-zinc-700 justify-start items-center gap-1">
-                  <div className="text-zinc-200 text-xs">{skill}</div>
+                  <div className="text-zinc-200 text-xs">{tag}</div>
                 </div>
               </div>
             ))}
           </div>
-          <div className="text-sm">{desciption}</div>
+          <div className="text-sm">{description.length > 80 ? `${description.slice(0, 80)}...` : description}</div>
         </div>
         <div className="flex flex-row gap-3 justify-evenly">
           {buttons}
--- app/components/home/Projects.js
+++ app/components/home/Projects.js
@@ -5,43 +5,53 @@ export default function Projects() {
     const projects = [
         {
             title: 'Project 1',
-            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            image: '/img/placeholder.webp',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: 'placeholder.webp',
             liveLink: 'https://google.com',
             githubLink: 'https://github.com',
-            skills: ['NextJS', 'Docker', 'React']
+            tags: ['NextJS', 'Docker', 'React'],
+            date: '11/7/2023',
+            show : true
         },
         {
-            title: 'Project 1',
-            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            image: '/img/placeholder.webp',
+            title: 'Project 2',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: 'placeholder.webp',
             liveLink: 'https://google.com',
             githubLink: 'https://github.com',
-            skills: ['NextJS', 'Docker', 'React']
+            tags: ['NextJS', 'Docker', 'React', 'MongoDB', 'NodeJS', 'Angular'],
+            date: '11/9/2023',
+            show : true
         },
         {
-            title: 'Project 1',
-            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            image: '/img/placeholder.webp',
+            title: 'Project 3',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: 'placeholder.webp',
             liveLink: 'https://google.com',
             githubLink: 'https://github.com',
-            skills: ['NextJS', 'Docker', 'React']
+            tags: ['NextJS', 'Docker', 'React'],
+            date: '11/5/2023',
+            show : false
         },
         {
-            title: 'Project 1',
-            desciption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            image: '/img/placeholder.webp',
+            title: 'Project 4',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            image: 'placeholder.webp',
             liveLink: 'https://google.com',
             githubLink: 'https://github.com',
-            skills: ['NextJS', 'Docker', 'React']
+            tags: ['NextJS', 'Docker', 'React'],
+            date: '11/24/2023',
+            show : true
         }
     ]
 
+    const sortedProjects = projects
+    .filter((project) => project.show) // Only show projects with show: true
+    .sort((a, b) => new Date(b.date) - new Date(a.date)); // Sort by date
+
     return (
-        <div id='projects' className='container
-        relative  h-full min-h-screen flex items-center flex-col gap-8
-        '>
-            <div className='mt-8 text-center self-center'>
+        <div id='projects' className='container relative my-16 h-full min-h-screen flex items-center flex-col gap-8'>
+            <div className='text-center self-center'>
                 <h2 className='text-2xl'>
                     My Projects
                 </h2>
@@ -53,11 +63,16 @@ export default function Projects() {
                 </p>
 
             </div>
-
+            {/* TODO Redo it into a slider */}
             <div className='flex flex-wrap w-full justify-center gap-4'>
-                {projects.map((project, index) => (
+                {/* projects decend based on date */}
+                {sortedProjects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
+
+                {/* {projects.map((project, index) => (
+                    <ProjectCard key={index} project={project} />
+                ))} */}
             </div>
         </div>
     )
