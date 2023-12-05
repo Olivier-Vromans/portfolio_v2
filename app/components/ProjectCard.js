8829e37db7bba22406c03b6d9a55566fa910b211
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-12-05 12:19:11 +0100

    edit smaller project card

--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard({ project }) {
   if (project === null) {
     return (
       <div className="flex flex-col max-w-xs max-h-full gap-4 sm:flex-row md:flex-col lg:flex-row xl:flex-col sm:max-w-md md:max-w-xs lg:max-w-sm xl:max-w-xs p-4 justify-evenly rounded-2xl shadow border border-neutral-50 border-opacity-10 backdrop-blur-sm bg-linearColor">
-          {/* Skeleton placeholder for the image */}
+        {/* Skeleton placeholder for the image */}
         <div className="relative w-full h-full sm:w-1/2 md:w-full lg:w-1/2 xl:w-full xl:h-1/2 bg-zinc-700 animate-pulse rounded-t-md lg:rounded-md">
         </div>
         <div className="flex-1 h-48 sm:h-full xl:h-56 flex flex-col justify-between gap-4">
@@ -63,7 +63,7 @@ export default function ProjectCard({ project }) {
 
 
   return (
-    <div className="flex flex-col max-w-xs max-h-full gap-4 sm:flex-row md:flex-col lg:flex-row xl:flex-col sm:max-w-md md:max-w-xs lg:max-w-2xl  xl:max-w-xs p-4 justify-evenly rounded-2xl shadow border border-neutral-50 border-opacity-10 backdrop-blur-sm bg-linearColor">
+    <div className="flex flex-col max-w-xs max-h-full gap-4 sm:flex-row md:flex-col lg:flex-row xl:flex-col sm:max-w-md md:max-w-xs lg:max-w-xl  xl:max-w-xs p-4 justify-evenly rounded-2xl shadow border border-neutral-50 border-opacity-10 backdrop-blur-sm bg-linearColor">
       <div className="relative w-full h-full aspect-square sm:w-1/2 md:w-full lg:w-1/2 xl:w-full xl:h-1/2 ">
         <Image
           src={image ? `/img/projects/${image}` : '/img/projects/placeholder.webp'}
@@ -73,7 +73,7 @@ export default function ProjectCard({ project }) {
         />
       </div>
       <div className="flex-1 h-48 sm:h-full xl:h-56 flex flex-col justify-between gap-4">
-        <div className='flex flex-col justify-between gap-4'>
+        <div className={`flex flex-col justify-between ${buttons ? "gap-4" : null}`}>
           <div className="text-xl">{title}</div>
           <div className="flex flex-wrap self-stretch justify-start items-start gap-2">
             {techStack.sort().slice(0, 5).map((tech, index) => (
@@ -84,8 +84,9 @@ export default function ProjectCard({ project }) {
               </div>
             ))}
           </div>
-          <div className="text-sm">{projectDetail.desired > 80 ? `${projectDetail.desired .slice(0, 80)}...` : projectDetail.desired }</div>
+          <div className="text-sm">{projectDetail.desired > 80 ? `${projectDetail.desired.slice(0, 80)}...` : projectDetail.desired}</div>
         </div>
+
         <div className="flex flex-row gap-3 justify-evenly">
           {buttons}
         </div>
